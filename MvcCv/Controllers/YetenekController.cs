2968df9e2cceb1c8d437503bad569410453d82fa
Sefa AŞIK <sefa.a1707@gmail.com>
2023-05-26 10:59:26 +0300

    Bölüm 6 sonu.

---
+++ MvcCv/Controllers/YetenekController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+
+namespace MvcCv.Controllers
+{
+    public class YetenekController : Controller
+    {
+        // GET: Yetenek
+
+        GenericRepository<TblYetenek> repo = new GenericRepository<TblYetenek>();
+        public ActionResult Index()
+        {
+            var yetenekler = repo.List();
+            return View(yetenekler);
+        }
+        [HttpGet]
+        public ActionResult YetenekEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YetenekEkle(TblYetenek y)
+        {
+            repo.TAdd(y);
+            return RedirectToAction("Index");
+        }
+        public ActionResult YetenekSil(int id)
+        {
+            var yetenek = repo.Find(x => x.ID == id);
+            repo.TDelete(yetenek);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult YetenekGetir(int id)
+        {
+            var yetenek = repo.Find(x => x.ID == id);
+            return View(yetenek);
+        }
+
+        [HttpPost]
+        public ActionResult YetenekGetir(TblYetenek t)
+        {
+            var y = repo.Find(x => x.ID == t.ID);
+            y.Yetenek = t.Yetenek;
+            y.Oran = t.Oran;
+            repo.TUpdate(y);
+            return RedirectToAction("Index");
+        }
+    }
+}
