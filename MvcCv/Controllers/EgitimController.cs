20218276feaaf036aa2c9211911663b2491993e7
Sefa AŞIK <sefa.a1707@gmail.com>
2023-05-26 16:15:04 +0300

    Bölüm 7 sonu.

---
+++ MvcCv/Controllers/EgitimController.cs
@@ -0,0 +1,67 @@
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class EgitimController : Controller
+    {
+        // GET: Egitim
+        GenericRepository<TblEgitim> repo = new GenericRepository<TblEgitim>();
+        public ActionResult Index()
+        {
+            var egitim = repo.List();
+            return View(egitim);
+        }
+
+        [HttpGet]
+        public ActionResult EgitimEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult EgitimEkle(TblEgitim p)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("EgitimEkle");
+            }
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult EgitimSil(int id)
+        {
+            var egitim = repo.Find(x => x.ID == id);
+            repo.TDelete(egitim);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult EgitimGetir(int id)
+        {
+            var egitim = repo.Find(x => x.ID == id);
+            return View(egitim);
+        }
+
+        [HttpPost]
+        public ActionResult EgitimGetir(TblEgitim t)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("EgitimGetir");
+            }
+            var y = repo.Find(x => x.ID == t.ID);
+            y.Baslik = t.Baslik;
+            y.AltBaslik1 = t.AltBaslik1;
+            y.AltBaslik2 = t.AltBaslik2;
+            y.GNO = t.GNO;
+            y.Tarih = t.Tarih;
+            repo.TUpdate(y);
+            return RedirectToAction("Index");
+        }
+    }
+}
